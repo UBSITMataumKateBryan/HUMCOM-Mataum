b1365de3183587e73c0c68356adb29bd0ffe2ecb
UBSITMataumKateBryan <98291707+UBSITMataumKateBryan@users.noreply.github.com>
2022-04-21 09:38:00 +0800

    intro to js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//this is innHTML sample
+/* multi-line comment in JS 
+document.getElementById("person").innerHTML="Lyndsy";
+is like this*/
+document.write("This is HUMCOM1");
+console.log("Hello, world!")
+console.log(5+23)
+//-----------call an alert from the script instead of the console----------
+alert("Hello this is a warning. \nYou can dismiss me by clicking in the OK button.")
